9b7a6c6c06ba29430633140ef0ed87e3ea71c69c
anastasija radulovic <ancideveloper@gmail.com>
2025-09-17 19:58:56 +0200

    set git ignore to ignore .env

--- client/src/layouts/rootLayout/RootLayout.jsx
+++ client/src/layouts/rootLayout/RootLayout.jsx
@@ -4,35 +4,35 @@ import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
+ if (!PUBLISHABLE_KEY) {
+   throw new Error("Missing Publishable Key");
+ }
 
-const queryClient = new QueryClient();
+ const queryClient = new QueryClient();
 
-const RootLayout = () => {
-  return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <QueryClientProvider client={queryClient}>
-        <div className="rootLayout">
-          <header>
-            <Link to="/" className="logo">
-              <img src="/logo.png" alt="" />
-              <span>LAMA AI</span>
-            </Link>
-            <div className="user">
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </div>
-          </header>
-          <main>
-            <Outlet />
-          </main>
-        </div>
-      </QueryClientProvider>
-    </ClerkProvider>
-  );
-};
+ const RootLayout = () => {
+   return (
+     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+       <QueryClientProvider client={queryClient}>
+         <div className="rootLayout">
+           <header>
+             <Link to="/" className="logo">
+               <img src="/logo.png" alt="" />
+               <span>LAMA AI</span>
+             </Link>
+             <div className="user">
+               <SignedIn>
+                 <UserButton />
+               </SignedIn>
+             </div>
+           </header>
+           <main>
+             <Outlet />
+           </main>
+         </div>
+       </QueryClientProvider>
+     </ClerkProvider>
+   );
+ };
 
 export default RootLayout;
